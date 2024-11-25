3ae18ea15a8062fdcdbec92368195bfb8f95daff
sumanthreddypam <sumanthreddypam@gmail.com>
2024-11-25 06:23:33 -0800

    adds SerializedName to all the variables

--- app/src/main/java/com/sam/flickr/data/dto/ImageEntity.kt
+++ app/src/main/java/com/sam/flickr/data/dto/ImageEntity.kt
@@ -1,10 +1,18 @@
 package com.sam.flickr.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class ImageEntity(
+    @SerializedName("description")
     val description: String,
+    @SerializedName("generator")
     val generator: String,
+    @SerializedName("items")
     val items: List<Item>,
+    @SerializedName("link")
     val link: String,
+    @SerializedName("modified")
     val modified: String,
+    @SerializedName("title")
     val title: String
 )
--- app/src/main/java/com/sam/flickr/data/dto/Item.kt
+++ app/src/main/java/com/sam/flickr/data/dto/Item.kt
@@ -3,15 +3,22 @@ package com.sam.flickr.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class Item(
+    @SerializedName("author")
     val author: String,
     @SerializedName("author_id")
     val authorId: String,
     @SerializedName("date_taken")
     val dateTaken: String,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("link")
     val link: String,
+    @SerializedName("media")
     val media: Media,
+    @SerializedName("published")
     val published: String,
+    @SerializedName("tags")
     val tags: String,
+    @SerializedName("title")
     val title: String
 )
